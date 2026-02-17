909c030ea875bda841415db9dda9a526a8c49f00
Mosrichard <mosrichard3@gmail.com>
2026-02-17 16:02:26 +0530

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,110 @@
+
+# -----------------------------
+# Default VPC & Subnet
+# -----------------------------
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# -----------------------------
+# IAM Role for SSM
+# -----------------------------
+
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-ssm-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
+# -----------------------------
+# Security Group (NO SSH)
+# -----------------------------
+
+resource "aws_security_group" "java_sg" {
+  name   = "java-app-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    description = "Java App Port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+  description = "SSH"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# -----------------------------
+# EC2 Instance (SSM Enabled)
+# -----------------------------
+
+resource "aws_instance" "server" {
+  ami                    = "ami-019715e0d74f695be"
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.java_sg.id]
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  tags = {
+    Name = "Web-Server"
+  }
+}
+
+# -----------------------------
+# ECR Repository
+# -----------------------------
+
+resource "aws_ecr_repository" "app_repo" {
+  name = "java-app-repo"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Name = "java-app-repo"
+  }
+}
