0ff6c2d5bc3f566f20c5ae14c25466bd4397b815
Mosrichard <mosrichard3@gmail.com>
2026-02-17 16:19:55 +0530

    added tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-unique-terraform-state-bucket-123"
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "tf-state-locks"
     encrypt        = true
   }
 }
